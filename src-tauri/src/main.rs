c1054b4ae5ce256d89030b6683211eedf1a4dd9c
qli <1787204269@qq.com>
2026-04-04 22:48:41 +0800

    feat: scaffold Tauri v2 project with all dependencies

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,15 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn project_compiles() {
+        assert!(true);
+    }
+}
