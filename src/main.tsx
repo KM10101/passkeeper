639e38d5cc1f3b2f4df4cf8decbc5b787203e813
qli <1787204269@qq.com>
2026-04-05 19:41:34 +0800

    feat: add Sonner toast system

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Toaster } from 'sonner';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Toaster position="bottom-right" richColors />
   </React.StrictMode>
 );
